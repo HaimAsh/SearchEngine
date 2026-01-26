72ff0b547231d49151d17fc5366e5e2a51b522a6
haimash <001haim@gmail.com>
2026-01-26 11:52:06 +0200

    CInvertedIndex - add search for query string

--- include/CInvertedIndex.h
+++ include/CInvertedIndex.h
@@ -5,9 +5,10 @@
 #ifndef SEARCHENGINE_CINVERTEDINDEX_H
 #define SEARCHENGINE_CINVERTEDINDEX_H
 
-#include "absl/container/flat_hash_map.h"
-#include <vector>
 #include <string>
+#include <vector>
+#include "absl/container/flat_hash_map.h"
+#include "CTokenizer.h"
 
 class CInvertedIndex
 {
@@ -15,11 +16,27 @@ public:
     void AddToken(absl::string_view token, uint32_t ID);
     const std::vector<uint32_t>& GetIDsOfToken(const absl::string_view token);
 
+    std::vector<uint32_t> Search(absl::string_view searchString);
+
     [[nodiscard]] size_t GetUniqueWordCount() const;
+
+    void AddTitle(uint32_t ID, absl::string_view title);
+
+    const std::string &GetTitle(uint32_t ID) const;
+
 private:
+
+    static void Intersect(const std::vector<uint32_t>& v1,
+                   const std::vector<uint32_t>& v2,
+                   std::vector<uint32_t>& result);
+
+    static void BreakString(absl::string_view str, std::vector<std::string>& result);
+
     absl::flat_hash_map<std::string, std::vector<uint32_t>> m_invertedIndex;
 
     std::vector<uint32_t> m_emptyVector;
+
+    std::vector<std::string> m_docTitles;
 };
 
 #endif //SEARCHENGINE_CINVERTEDINDEX_H
