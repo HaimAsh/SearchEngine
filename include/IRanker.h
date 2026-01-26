70c89ce8167099c9ff793e57b0408290536e02a0
haimash <001haim@gmail.com>
2026-01-26 15:15:10 +0200

    IRanker.h - add interface to define ranking classes

---
+++ include/IRanker.h
@@ -0,0 +1,21 @@
+//
+// Created by haimash on 26/01/2026.
+//
+
+#ifndef SEARCHENGINE_IRANKER_H
+#define SEARCHENGINE_IRANKER_H
+
+#include <vector>
+#include <cstdint>
+
+class IRanker {
+public:
+    virtual ~IRanker() = default;
+
+    // Returns a sorted list of Document IDs
+    virtual std::vector<uint32_t> Rank(
+        const std::vector<std::pair<uint32_t, uint32_t>>& matches,
+        size_t totalDocs) const = 0;
+};
+
+#endif //SEARCHENGINE_IRANKER_H
